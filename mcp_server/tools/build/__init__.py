983ea514405c19d10f761181f2d938dec9390dd7
Leonid <79429232+LeonidYasin@users.noreply.github.com>
2026-07-01 19:14:59 +0300

    Add build package with log analysis tools

---
+++ mcp_server/tools/build/__init__.py
@@ -0,0 +1,10 @@
+"""Build tools for Android and iOS."""
+
+# Import decorated functions so ToolRegistry can find them
+from mcp_server.tools.build.build_logs import get_android_build_error
+from mcp_server.tools.build.build_logs import get_ios_build_error
+
+__all__ = [
+    "get_android_build_error",
+    "get_ios_build_error",
+]
