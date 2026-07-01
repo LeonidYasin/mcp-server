784aec17778067219ccc7da6ab64ac1c51fca4ee
Leonid <79429232+LeonidYasin@users.noreply.github.com>
2026-07-01 06:40:35 +0300

    Remove old github tools init

--- mcp_server/tools/github/__init__.py
+++
@@ -1,6 +0,0 @@
-"""GitHub API tools."""
-
-from mcp_server.tools.github.client import GitHubClient
-from mcp_server.tools.github.delete_file import delete_file
-
-__all__ = ["GitHubClient", "delete_file"]
